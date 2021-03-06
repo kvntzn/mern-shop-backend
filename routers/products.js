22178116f0bc7f6f517913728a5b020ea63cbbad
= <kevintuazon45@gmail.com>
2021-03-06 10:49:10 +0800

    update product and routes

--- routers/products.js
+++ routers/products.js
@@ -1,7 +1,6 @@
+const { Product } = require(".models/product");
 const express = require("express");
 const router = express.Router();
-
-const Product = require("../models/product");
 
 router.get(`/`, async (req, res) => {
   const productList = await Product.find();
