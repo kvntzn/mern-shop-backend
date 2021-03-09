823517502f08e1cb40ef6d07cb755bc5eb156a58
= <kevintuazon45@gmail.com>
2021-03-09 11:26:57 +0800

    updated user schema

--- routers/users.js
+++ routers/users.js
@@ -4,4 +4,26 @@ const router = express.Router();
 
 router.get(`/`, async (req, res) => {});
 
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    image: req.body.image,
+    street: req.body.street,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+  });
+  user = await user.save();
+
+  if (!user) {
+    return res.status(404).send("the category cannot be created");
+  }
+
+  res.send(user);
+});
+
 module.exports = router;
