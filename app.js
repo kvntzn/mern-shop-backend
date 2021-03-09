088723d898e8b06183f64f49de25689435e22b4c
= <kevintuazon45@gmail.com>
2021-03-09 11:26:44 +0800

    setup user schema

--- app.js
+++ app.js
@@ -14,13 +14,16 @@ const api = process.env.API_URL;
 
 const productsRouter = require("./routers/products");
 const categoriesRouter = require("./routers/categories");
+const usersRouter = require("./routers/users");
 
 //middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
+ßßßß;
 
 app.use(`${api}/products`, productsRouter);
 app.use(`${api}/categories`, categoriesRouter);
+app.use(`${api}/users`, usersRouter);
 
 mongoose
   .connect(process.env.CONNECTION_STRING, {
