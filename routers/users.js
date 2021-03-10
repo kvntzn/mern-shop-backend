db8ee2c745c807c37728232b94b56520703bb2d3
= <kevintuazon45@gmail.com>
2021-03-10 10:44:51 +0800

    exclude authorize paths added register

--- routers/users.js
+++ routers/users.js
@@ -39,7 +39,29 @@ router.post("/", async (req, res) => {
   user = await user.save();
 
   if (!user) {
-    return res.status(404).send("the category cannot be created");
+    return res.status(404).send("the user cannot be created");
+  }
+
+  res.send(user);
+});
+
+router.post("/register", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    image: req.body.image,
+    street: req.body.street,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+  });
+  user = await user.save();
+
+  if (!user) {
+    return res.status(404).send("the user cannot be created");
   }
 
   res.send(user);
