f1afa9a39d42619d8297523c61e56aada6955b4f
= <kevintuazon45@gmail.com>
2021-03-12 10:33:10 +0800

    added counts

--- routers/orders.js
+++ routers/orders.js
@@ -120,4 +120,25 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.get("/get/totalsales", async (req, res) => {
+  const totalSales = await Order.aggregate({
+    $group: { _id: null, totalSales: { $sum: "$totalPrice" } },
+  });
+
+  if (!totalSales) {
+    return res.status(400).send("The order sales cannot be generated");
+  }
+
+  return res.send({ totalSales: totalSales.pop().totalSalesß });
+});
+
+router.get("/get/count", async (req, res) => {
+  const orderCount = await Order.countDocuments((count) => count);
+
+  if (!orderCount) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send({ orderCount });
+});
 module.exports = router;
